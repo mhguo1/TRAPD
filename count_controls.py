89db9e655e2c634d2d30afe8096d22c860f087e2
mhguo1 <mhguo1@gmail.com>
2018-01-30 18:30:19 -0500

    Update count_controls.py

--- count_controls.py
+++ count_controls.py
@@ -12,7 +12,7 @@ parser.add_option("-v", "--vcffile", action="store",dest="vcffilename")
 parser.add_option("-o", "--outfile", action="store",dest="outfilename", default="out.txt")
 
 parser.add_option("--snpformat", action="store",dest="snpformat", default="VCFID")
-parser.add_option("--pop", action="append",dest="pop", default="ALL")
+parser.add_option("--pop", action="store",dest="pop", default="ALL")
 
 #Optional Filters
 parser.add_option("--pass", action="store_true", dest="passfilter")
@@ -25,6 +25,12 @@ if not options.snpfilename:   # if filename is not given
 
 if not options.vcffilename:   # if filename is not given
     parser.error('A vcf file is needed')
+
+#Parse populations
+if options.pop is not None:
+	pops=str(options.pops).split(',')
+else:
+	pops=["ALL"]
 
 
 def makesnplist(snpfile):
@@ -43,26 +49,27 @@ def makesnplist(snpfile):
 	snp_file.close()
 
 def extractcounts(pops, vcfline, max_ac, max_af):
-	ac_out=(";"+vcfline).split((";AC="))[1].split(";")[0]
-	af_out=(";"+vcfline).split((";AF="))[1].split(";")[0]
+	ac_out=float((";"+vcfline).split((";AC="))[1].split(";")[0])
+	af_out=float((";"+vcfline).split((";AF="))[1].split(";")[0])
 	ac_hom_out=(";"+vcfline).split((";Hom="))[1].split(";")[0]
 
-	if (ac_out>max_ac) or (af_out>max_af):
-		return [[],[]]
-	elif (options.pop is not None) or ("ALL" not in options.pop):
+	if (ac_out>float(max_ac)) or (af_out>float(max_af)):
+		ac_out=0
+		ac_hom_out=0
+	elif "ALL" not in pops:
 		for p in range(0, len(pops), 1):
 			temp_pop=pops[p]
 			ac_out=ac_out+int((";"+vcfline).split((";AC"+pop+"="))[1].split(";")[0])
 			ac_hom_out=ac_hom_out+int((";"+vcfline).split((";Hom_"+pop+"="))[1].split(";")[0])
-		return [ac_out, ac_hom_out]
+	return [ac_out, ac_hom_out]
 
 
-def sumcount(snplist, snptable):
+def sumcount(genesnps, snptable):
 	ac_sum=0
 	ac_hom_sum=0
-	for s in range(0, len(snplist), 1):
-		if snplist[s] in snptable:
-			tempsnp=snplist[s]
+	for s in range(0, len(genesnps), 1):
+		if genesnps[s] in snptable:
+			tempsnp=genesnps[s]
 			ac_sum=ac_sum+int(snptable[tempsnp][1])
 			ac_hom_sum=ac_hom_sum+int(snptable[tempsnp][2])
 	return [ac_sum, ac_hom_sum]
@@ -73,11 +80,10 @@ allsnplist=makesnplist(options.snpfilename)
 #Make a hashtable with keys as each SNP, and stores a list of indices of carriers for that SNP
 count_table={} 
 
+
 #Open vcf file
-if str(options.vcffilename)[-3:]==".gz":
-	vcffile=gzip.open(options.vcffilename, "rb")
-else:
-	vcffile=open(options.vcffilename, "r")
+vcffile=gzip.open(options.vcffilename, "rb")
+
 for line_vcf1 in vcffile:
 	line_vcf=line_vcf1.rstrip().split('\t')
 	if line_vcf[0][0]!="#":
@@ -94,6 +100,10 @@ vcffile.close()
 
 #Test by writing output
 outfile=open(options.outfilename, "w")
+##for x in count_table:
+##	outfile.write(str(count_table[x][0])+"\t"+str(count_table[x][1])+"\t"+str(count_table[x][2])+"\n")
+##outfile.close()
+
 snpfile=open(options.snpfilename, "r")
 for line_s1 in snpfile:
 	line_s=line_s1.rstrip('\n').split('\t')
@@ -104,4 +114,4 @@ for line_s1 in snpfile:
 outfile.close()
 snpfile.close()
 
-
+##python count_control.py -v gnomad.test.vcf.gz -o counts.gnomad.txt -s test.out.txt --snpformat CHRPOSREFALT --pop ALL
