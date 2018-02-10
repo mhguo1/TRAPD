25b0d3565838989908bf7913d2c8b934e7663630
mhguo1 <mhguo1@gmail.com>
2018-02-10 14:36:53 -0500

    Update merge_snp_file.py

--- code/merge_snp_file.py
+++ code/merge_snp_file.py
@@ -23,24 +23,24 @@ snptable={}
 for f in range(0,len(snpfilelist),1):
     tempfile=open(snpfilelist[f], "r")
     for line_s1 in tempfile:
-          line_s=line_s1.rstrip().split('\t')
-          if line_s[0][0]!="#":
-                gene=line_s[0]
-                snps=line_s[1].split(',')
-                if gene in snptable:
-                      snptable[gene][1]=snptable[gene][1]+snps
-                else:
-                      snptable[gene]=[gene, snps]
-     tempfile.close()
+        line_s=line_s1.rstrip().split('\t')
+        if line_s[0][0]!="#" and len(line_s)>0:
+            gene=line_s[0]
+            snps=line_s[1].split(',')
+            if gene in snptable:
+                snptable[gene][1]=snptable[gene][1]+snps
+            else:
+                snptable[gene]=[gene, snps]
+    tempfile.close()
      
 #Write Output
 outfile=open(options.outfilename, "w")
 outfile.write("#GENE\tSNPS\n")
 for x in snptable:
-        if len(x)>0:
-        #Read through hash table and print out variants
-                snp_out=','.join(snptable[x][1])
-                outfile.write(str(x)+"\t"+snp_out+"\n")
+    if len(x)>0:
+    #Read through hash table and print out variants
+        snp_out=','.join(snptable[x][1])
+        outfile.write(str(x)+"\t"+snp_out+"\n")
 outfile.close()
                     
                 
