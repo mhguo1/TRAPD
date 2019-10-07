8b61376a2d841b23be7336f1259d4a6df9837e78
mhguo1 <mhguo1@gmail.com>
2019-10-07 19:32:46 -0400

    Update test2.py

--- code/test2.py
+++ code/test2.py
@@ -305,41 +305,7 @@ def get_operator_fn(op):
 #Create empty snptable
 snptable={}
 
-#subset bedfile
-#if options.bedfilename is not None:
-#	vcffile=gzip.open(options.vcffilename, "rb")
-#	all_snp_list=[]
-#	all_snp_list={}
-#	bed_snp_list=[]
-#	for line_v1 in vcffile:
-#		line_v=line_v1.rstrip().split('\t')
-#		if line_v[0][0]!="#":
-#	#		all_snp_list.append(str(line_v[0]).lower().replace("chr", "")+":"+str(line_v[1]))
-#			chr=str(line_v[0]).lower().replace("chr", "")
-#			if chr not in all_snp_list:
-#				all_snp_list[chr]=[chr, []]
-#			all_snp_list[chr][1].append(line_v[1])
-	
-#	vcffile.close()
-	#all_snp_list=set(all_snp_list)
-	
-#	if str(options.bedfilename).endswith(".gz") is True:
-#		bed=gzip.open(options.bedfilename, "rb")
-#	else:
-#		bed=open(options.bedfile, "r")
-	
-#        for line_b1 in bed:
-#                line_b=line_b1.rstrip().split('\t')
-#                chr=str(line_b[0]).lower().replace("chr", "")
-#                if chr in all_snp_list:
-#                        temp=[int(i) for i in all_snp_list[chr][1]]
-#                        if len(temp)>0:
-#                                temp_pos=list(set(range(int(line_b[1])+1, int(line_b[2])+1))& set(temp))
-#                                if len(temp_pos)>0:
-#                                        bed_snp_list=bed_snp_list+list([chr+":"+s for s in [str(i) for i in temp_pos]])
-#                               all_snp_list[chr][1]=[n for n in temp if int(n)>int(line_b[2])]
-#        bed.close()
-
+#read in bedfile
 if options.bedfilename is not None:
 	if str(options.bedfilename).endswith(".gz") is True:
 		bed=gzip.open(options.bedfilename, "rb")
@@ -350,12 +316,11 @@ if options.bedfilename is not None:
        	for line_b1 in bed:
                 line_b=line_b1.rstrip().split('\t')
                 chr=str(line_b[0]).lower().replace("chr", "")
-		if chr in bed_lower:
+		if chr not in bed_lower:
 			bed_lower[chr]=[chr, []]
 			bed_upper[chr]=[chr, []]
-		bed_lower[chr][1].append(int(line_b[1]+1)
-		bed_upper[chr][1].append(int(line_b[2])
-		
+                bed_lower[chr][1].append(int(line_b[1])+1)
+                bed_upper[chr][1].append(int(line_b[2]))
 	bed.close()	
 
 vcffile=gzip.open(options.vcffilename, "rb")
@@ -381,15 +346,14 @@ for line_vcf1 in vcffile:
 				keep=0
 		if "," in line_vcf[4]:
 			keep=0
+			
+		#Subset on bedfile
 		if options.bedfilename is not None:
-		#	snp=str(line_vcf[0])+":"+str(line_vcf[1])
-		#	if snp not in bed_snp_list:
-		#		keep=0
-			chr=str(line_v[0]).lower().replace("chr", "")
-			temp_index=bisect.bisect(bed_lower[chr][1], int(line_v[1]))-1
+			chr=str(line_vcf[0]).lower().replace("chr", "")
+			temp_index=bisect.bisect(bed_lower[chr][1], int(line_vcf[1]))-1
 			if temp_index<0:
 				keep=0	 
-			elif int(line_v[1])>bed_upper[chr][1][temp_index]:
+			elif int(line_vcf[1])>bed_upper[chr][1][temp_index]:
 				keep=0
 					 
  #Go through INFO field filters
